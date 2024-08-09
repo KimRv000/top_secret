220c3d30c0dcb821cda7b1c270962b9ce080ccce
Kimberly Rv <elliis.rv@gmail.com>
2024-08-08 21:42:17 -0600

    final version

--- main.js
+++ main.js
@@ -1,73 +1,101 @@
-/*
+
 inputText = document.getElementById("inputText");
-console.log(inputText)
+//console.log(inputText)
+
+let copied_text = "";
 
 encodeButton = document.getElementById("encodeButton");
 decodeButton = document.getElementById("decodeButton");
 outputText = document.getElementById("outputText");
 outputContainer = document.getElementById("outputContainer");
 copyButton = document.getElementById("copyButton");
+deleteButton = document.getElementById("deleteButton");
+instructionsText = document.getElementById("instructionsInput");
 
 encodeButton.addEventListener("click", () => {
-    console.log(inputText.value);
+    copied_text = encode(inputText.value);
+    //console.log(inputText.value);
     outputText.innerText = encode(inputText.value);
-    outputContainer.style.visibility = "visible";
-    //copyButton.style.visibility = "visible";
+    if (copied_text !== "") {
+        outputContainer.style.visibility = "visible";
+        copyButton.style.visibility = "visible";
+        encodeButton.style.visibility = "hidden";
+        decodeButton.style.visibility = "hidden";
+        deleteButton.style.visibility = "visible"
+        instructionsText.style.visibility = "hidden"
+    }
 })
 decodeButton.addEventListener("click", () => {
-    console.log(inputText.value);
-    outputText.innerText = inputText.value;
-    outputContainer.style.visibility = "visible";
-    //copyButton.style.visibility = "visible";
+    copied_text = decode(inputText.value);
+    //console.log(outputText.value);
+    outputText.innerText = decode(inputText.value);
+    if (copied_text !== "") {
+        outputContainer.style.visibility = "visible";
+        copyButton.style.visibility = "visible";
+        encodeButton.style.visibility = "hidden";
+        decodeButton.style.visibility = "hidden";
+        deleteButton.style.visibility = "visible"
+        instructionsText.style.visibility = "hidden"
+
+    }
 })
 
-//------------------------------Crear funcion para codificar------------------------------//
+copyButton.addEventListener("click", () => {
+    //console.log(copied_text)
+    if (copied_text !== "") {
+        navigator.clipboard.writeText(copied_text) 
+            .then(() => {
+                console.log("Texto copiado: ", copied_text);
+                alert("Texto copiado al portapapeles!");
+            })
+            .catch(err => {
+                console.error("Error al copiar el texto: ", err);
+                alert("Error al copiar el texto.");
+            });
+    }
+
+})
+
+deleteButton.addEventListener("click", () => {
+    inputText.value = "";
+    console.log("delete")
+    outputContainer.style.visibility = "hidden";
+    copyButton.style.visibility = "hidden";
+    encodeButton.style.visibility = "visible";
+    decodeButton.style.visibility = "visible";
+    deleteButton.style.visibility = "hidden"
+    instructionsText.style.visibility = "visible"
+
+})
+
+//---------------------------- functions ----------------------------------//
+
+let code_keys = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
 
 function encode(text) {
-    
-    lower_text = text.toLowerCase();
-    console.log(lower_text)
-    
-    final_text = "";
 
-    for (let i = 0; i < lower_text.length; i++) {
-        if (lower_text[i] === "e") {
-            final_text += "enter";
-        } else if (lower_text[i] === "i") {
-            final_text += "imes";
-        } else if (lower_text[i] === "a") {
-            final_text += "ai";
-        } else if (lower_text[i] === "o") {
-            final_text += "ober";
-        } else if (lower_text[i] === "u") {
-            final_text += "ufat";
-        }else{
-            final_text += lower_text[i]
+    let lower_text = text.toLowerCase();
+
+    for (let i = 0; i < code_keys.length; i++) {
+        if (lower_text.includes(code_keys[i][0])) {
+            lower_text = lower_text.replaceAll(code_keys[i][0], code_keys[i][1])
         }
     }
 
-    console.log(final_text)
-    return final_text
+    return lower_text
+
 }
-
-*/
-
-//------------------------------Crear funcion para decodificar------------------------------//
 
 function decode(text) {
-    
-    lower_text = text.toLowerCase();
-    
-    final_text = "";
 
-    array_text = lower_text.split(' ');
+    let lower_text = text.toLowerCase();
 
-    array_text.forEach(word => {
-        console.log(word)
-    });
-    
+    for (let i = 0; i < code_keys.length; i++) {
+        if (lower_text.includes(code_keys[i][1])) {
+            lower_text = lower_text.replaceAll(code_keys[i][1], code_keys[i][0])
+        }
+    }
 
-    console.log(array_text)
-    //return final_text
+    return lower_text
+
 }
-decode("henterllober my imes")
